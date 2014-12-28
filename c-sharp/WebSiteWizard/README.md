7133078e3af0f1710ecab978d454971709fbf2cc
Henrique Dias <hacdias@gmail.com>
2014-12-28 14:31:12 +0000

    Send a C# script.

---
+++ c-sharp/WebSiteWizard/README.md
@@ -0,0 +1,39 @@
+#Web Site Wizard
+
+My web programming environment has the following setup to the apache virtual hosts:
+
+```Apache
+<Virtualhost *:80>
+    VirtualDocumentRoot "D:/Dev/www/%1/wwwroot"
+    ServerName sites.dev
+    ServerAlias *.dev
+    UseCanonicalName Off
+</Virtualhost>
+```
+
+Here is everything automated: I only have to create the folders to access their content with an URL similar to this: ```*.dev``` .
+
+So if I want to create the ```hello.dev``` site, I have to create the folder ```D:/Dev/www/hello/wwwroot``` and add the following information to the
+
+But when I want to create a new site I have to create the folders and add the following information to the Windows hosts file:
+
+```
+127.0.0.1   hello.dev
+```
+
+As Windows doesn't support wildcards in its hosts file, I can't simply write the following:
+
+```
+127.0.0.1   *.dev
+```
+
+And repeat the same process every time is boring so I've created this C# console application that creates the folders and adds the information to the Windows hosts file.
+
+You just have to change the following lines into "Program.cs" and recompile it.
+
+```c-sharp
+string devDir = @"D:\Dev\www",
+    hostsFile = @"C:\Windows\System32\drivers\etc\hosts",
+```
+
+It's simple, easy and useful.
---
+++ c-sharp/WebSiteWizard/WebSiteWizard/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace WebSiteWizard
+{
+    class Program
+    {
+        static void Main()
+        {
+            string devDir = @"D:\Dev\www",
+                hostsFile = @"C:\Windows\System32\drivers\etc\hosts",
+                hostsContent,
+                hostsToAdd,
+                projectName,
+                projectDir,
+                wwwDir;
+
+            string[] title = new string[] {
+                @"   ____                          __               ____        __             ",
+                @"  /\  _`\                       /\ \__           /\  _`\   __/\ \__          ",
+                @"  \ \ \/\_\  _ __    __     __  \ \ ,_\    __    \ \,\L\_\/\_\ \ ,_\    __   ",
+                @"   \ \ \/_/_/\`'__\/'__`\ /'__`\ \ \ \/  /'__`\   \/_\__ \\/\ \ \ \/  /'__`\ ",
+                @"    \ \ \L\ \ \ \//\  __//\ \L\.\_\ \ \_/\  __/     /\ \L\ \ \ \ \ \_/\  __/ ",
+                @"     \ \____/\ \_\\ \____\ \__/.\_\\ \__\ \____\    \ `\____\ \_\ \__\ \____\",
+                @"      \/___/  \/_/ \/____/\/__/\/_/ \/__/\/____/     \/_____/\/_/\/__/\/____/"
+            };
+
+            Console.WriteLine();
+
+            for (int i = 0; i < title.Length; i++)
+            {
+                Console.WriteLine(title[i]);
+            }
+
+            Console.WriteLine();
+            Console.Write("Please insert the name of your new web project: ");
+
+            projectName = Console.ReadLine();
+            projectDir = System.IO.Path.Combine(devDir, projectName);
+            wwwDir = System.IO.Path.Combine(projectDir, "wwwroot");
+
+            Console.WriteLine();
+
+            if (Directory.Exists(projectDir) || Directory.Exists(wwwDir))
+            {
+                Console.WriteLine("The path " + wwwDir + " already exists.");
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(projectDir);
+                Console.WriteLine(projectDir + " created.");
+                System.IO.Directory.CreateDirectory(wwwDir);
+                Console.WriteLine(wwwDir + " created");
+
+                hostsContent = System.IO.File.ReadAllText(hostsFile);
+
+                hostsToAdd = "\n127.0.0.1\t" + projectName + ".dev";
+                hostsToAdd += "\n127.0.0.1\twww." + projectName + ".dev";
+
+                hostsContent += hostsToAdd;
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(hostsFile);
+                file.WriteLine(hostsContent);
+                file.Close();
+
+                Console.WriteLine();
+                Console.WriteLine("The following entries were added to your hosts file:");
+                Console.WriteLine(hostsToAdd);
+            }
+
+            Console.WriteLine();
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
