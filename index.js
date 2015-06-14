d9995bd8ec01616bc45a17c136f31b6d1aeaecf7
Henrique Dias <hacdias@gmail.com>
2015-06-14 11:18:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+function checkIf(thing, is, where, what) {
+  if (typeof thing === is) {
+    if (typeof where === 'undefined') {
+      return true;
+    } else {
+      throw new SyntaxError('You have defined more than one ' + what);
+    }
+  } else {
+    return;
+  }
+}
+
+function parseArguments(args) {
+  var opts = {};
+
+  for (var i = args.length - 1; i > 0; i--) {
+    if (checkIf(args[i], 'function', opts.callback, 'callback function')) {
+      opts.callback = args[i];
+      continue;
+    }
+
+    if (checkIf(args[i], 'object', opts.args, 'array of arguments')) {
+      opts.args = args[i];
+      continue;
+    }
+
+    if (checkIf(args[i], 'string', opts.cwd, 'working directory')) {
+      opts.cwd = args[i];
+    }
+  }
+
+  opts.cwd = (typeof opts.cwd === 'undefined') ? process.cwd() : opts.cwd;
+  opts.args = (typeof opts.args === 'undefined') ? [] : opts.args;
+
+  return opts;
+}
+
+module.exports = function (cmd /*, args, cwd, callback */) {
+  var os = require('os'),
+      path = require('path'),
+      childProcess = require('child_process'),
+      isWindows = os.platform() === 'win32',
+      opts = parseArguments(arguments),
+      args = opts.args,
+      cwd = opts.cwd,
+      callback = opts.callback;
+
+  if (isWindows) {
+    args.unshift(cmd);
+
+    args = [
+      '/s',
+      '/c',
+      args.join(' ')
+    ];
+
+    cmd = 'cmd.exe';
+  }
+
+  var child = childProcess.spawn(cmd, args, {
+    cwd: cwd,
+    env: process.env,
+    stdio: 'inherit'
+  });
+
+  child.on('close', function(code) {
+    callback();
+  });
+};
---
+++ readme.md
@@ -0,0 +1,36 @@
+# interactive-command
+
+[![Build](https://img.shields.io/travis/hacdias/interactive-command.svg?style=flat-square)][1]
+[![Latest Version](https://img.shields.io/npm/v/interactive-command.svg?style=flat-square)][1]
+[![Downloads per Month](https://img.shields.io/npm/dm/interactive-command.svg?style=flat-square)][1]
+[![License](https://img.shields.io/npm/l/interactive-command.svg?style=flat-square)](http://opensource.org/licenses/MIT)
+
+Run a command interactively on all operating systems, including Windows. It uses the ```child_process.spawn``` function to read the input and the output.
+
+
+## Install
+
+```sh
+$ npm install --save interactive-command
+```
+
+
+## Usage
+
+```js
+var system = require('interactive-command.');
+system(cmd, [args, cwd, callback])
+```
+
+Where:
+
+* ```cmd``` is the command;
+* ```args``` is an array of arguments for the command;
+* ```cwd``` is the working directory where you want the execute the command;
+* ```callback``` is the... callback.
+
+## License
+
+MIT © [Henrique Dias](http://henriquedias.com)
+
+[1]: https://www.npmjs.com/package/interactive-command.
